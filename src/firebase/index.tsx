42669875c6ef70bd5fee37fce20a5b21079d2ef8
Tate Reynolds <tate.reynolds@gmail.com>
2023-04-05 12:03:34 -0600

    added env

--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -9,14 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxz9nU4eWZ1VK0L6-I7D0AQ9CMu-0PDJ8",
-  authDomain: "wedding-rsvp-zate.firebaseapp.com",
-  projectId: "wedding-rsvp-zate",
-  storageBucket: "wedding-rsvp-zate.appspot.com",
-  databaseURL: "https://wedding-rsvp-zate-default-rtdb.firebaseio.com",
-  messagingSenderId: "554105169785",
-  appId: "1:554105169785:web:4a1b4f51f60cd317a6b1a8",
-  measurementId: "G-14VL06ZLNN"
+  //.env
 };
 
 // Initialize Firebase
